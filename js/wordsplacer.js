b6479c60dba1ac6d42cb0d8f189341e97e98f2d9
rettpop <rettpop@leftbit.com>
2016-08-22 19:54:31 +0300

    Placement is working

--- js/wordsplacer.js
+++ js/wordsplacer.js
@@ -30,13 +30,13 @@ function placeWords(inWordsArr)
 
 	 */
 	
-	var ROTATION = {
+	var Rotations = {
 			HOR: {value: 0, name : "Horizontal"},
 			VERT: {value: 1, name : "Vertical"},
-			DIAG: {value: 2, name : "Diagonal"},
+			DIAG: {value: 2, name : "Diagonal"}
 	};
 	
-	Object.freeze(ROTATION);
+	Object.freeze(Rotations);
 	
 	var Position = function () { return {
 			startX:-1,
@@ -70,36 +70,41 @@ function placeWords(inWordsArr)
 
     // Create array of unplaced words
     // Create array of placed words
-	var placedWords, notplacesWords, positions = [];
+	var placedWords = []; 
+	var notplacesWords = [];
+	var positions = new Map();
 	
 	// for each word
 	for( var oneWord of wordsArray )
 	{
+		positions.set(Rotations.HOR, []);
+		positions.set(Rotations.VERT, []);
+		positions.set(Rotations.DIAG, []);
 		// for each rotation
-		for (let oneRotation of Rotations )
+		for (let oneRotation of [Rotations.HOR, Rotations.VERT, Rotations.DIAG] )
 		{
 			// running through the matrix and trying to place
-			for(var idx = 0; idx < matrix.length; idx++)
+			for(let idx = 0; idx < matrix.length; idx++)
 			{
-				for(var idy = 0; idy < matrix[idx].length; idx++)
+				for(let idy = 0; idy < matrix[idx].length; idy++)
 				{
-					var posX = idx;
-					var posY = idy;
+					let posX = idx;
+					let posY = idy;
 
                     //TODO: Optimize check
                     // check if word can fit rest of cells
-                    if( rotation == Rotations.HOR &&
+                    if( oneRotation == Rotations.HOR &&
                         (posX + oneWord.length) > MATRIX_SIZE )
                     {
                         continue;
                     }
-                    if( rotation == Rotations.VERT &&
+                    if( oneRotation == Rotations.VERT &&
                         (posY + oneWord.length) > MATRIX_SIZE )
                     {
                         continue;
                     }
 
-                    if( rotation == Rotations.DIAG &&
+                    if( oneRotation == Rotations.DIAG &&
                         ((posX + oneWord.length) > MATRIX_SIZE ||
                          (posY + oneWord.length) > MATRIX_SIZE) )
                     {
@@ -108,20 +113,20 @@ function placeWords(inWordsArr)
 
 					// walk through all word letters and check if they can be placed in the matrix
                     var canBePlaced = true;
-					for( var idxLetter = 0; idxLetter < oneWord.length; idxLetter++ )
+					for( let idxLetter = 0; idxLetter < oneWord.length; idxLetter++ )
 					{
-                        const matrixCell = matrixp[posX][posY];
+                        const matrixCell = matrix[posX][posY];
 
                         if( matrixCell != "" &&
-                            matrixCell != oneWord[idxLetter] )
+                            matrixCell != oneWord.text.charAt(idxLetter) )
                         {
                             canBePlaced = false;
                             break;
                         }
 
                         // increase matrix coords
-                        posX += 1 * (oneRotation == Rotations.HOR_ROTATION || oneRotation == Rotations.DIAG_ROTATION) ? 1:0;
-                        posY += 1 * (oneRotation == Rotations.VERT_ROTATION || oneRotation == Rotations.DIAG_ROTATION) ? 1:0;
+                        posX += 1 * (oneRotation == Rotations.HOR || oneRotation == Rotations.DIAG) ? 1:0;
+                        posY += 1 * (oneRotation == Rotations.VERT || oneRotation == Rotations.DIAG) ? 1:0;
 					}
 
                     // if no obstacles were found, add position to available positions array and continue
@@ -133,15 +138,93 @@ function placeWords(inWordsArr)
                         position.rotation = oneRotation;
                         position.length = oneWord.length;
 
-                        positions.push(position)
+                        positions.get(oneRotation).push(position);
                     }
 				} //for vary
 			} //for(var idx = 0; idx < matrix.length; idx++)
 		} //for (let oneRotation of Rotations )
-	} //for( var oneWord of wordsArray )
 
-    // if we have positions available, choose one and place word there
-    if(positions.length > 0){
-        
-    }
+		//TODO: move to variable rotations amount 
+		// if we have positions available, choose one and place word there
+		let cntHor = positions.get(Rotations.HOR).length;
+		let cntVert = positions.get(Rotations.VERT).length;
+		let cntDiag = positions.get(Rotations.DIAG).length;
+			
+		//if we have at least one position... 
+	    if( (cntHor + cntVert + cntDiag) > 0 )
+	    {
+	    	// choose one of available rotations.
+	    	// need as, i.e., DIAG rotation has less amount and being selected less than others
+			let posArr = [];
+			if ( positions.get(Rotations.HOR).length > 0 ) {
+				posArr.push(positions.get(Rotations.HOR));
+			}
+			if ( positions.get(Rotations.VERT).length > 0 ) {
+				posArr.push(positions.get(Rotations.VERT));
+			}
+			if ( positions.get(Rotations.DIAG).length > 0 ) {
+				posArr.push(positions.get(Rotations.DIAG));
+			}
+
+			let selRotationIdx = Math.floor(Math.random() * posArr.length + 1); 
+
+	    	// from rotation choosen select one of positions 
+	        let randIdx = Math.floor(Math.random() * posArr[selRotationIdx - 1].length + 1);
+	        oneWord.position = posArr[selRotationIdx - 1][randIdx - 1];
+	        placedWords.push(oneWord);
+	        
+	        // fill the matrix with current word
+        	let posX = oneWord.position.startX;
+        	let posY = oneWord.position.startY;
+	        for( let idx = 0; idx < oneWord.length; idx++ ) 
+	        {
+	        	matrix[posX][posY] = oneWord.text.charAt(idx);
+
+	        	//TODO: Refactor
+                // increase matrix coords
+                posX += 1 * (oneWord.position.rotation == Rotations.HOR || oneWord.position.rotation == Rotations.DIAG) ? 1:0;
+                posY += 1 * (oneWord.position.rotation == Rotations.VERT || oneWord.position.rotation == Rotations.DIAG) ? 1:0;
+	        }
+	    }
+	    else
+	    {
+	    	notplacesWords.push(oneWord);
+	    }
+	} //for( var oneWord of wordsArray )
+	
+	console.log("Words placed: " + placedWords.length);
+	alert("Words notplaced: " + notplacesWords.length);
+	let alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".split(""); 
+	
+	// fill matrix gaps with arbitrary letters
+	for( let idx=0; idx < MATRIX_SIZE; idx++ ) {
+		for( let idy=0; idy < MATRIX_SIZE; idy++ ) {
+			if( matrix[idx][idy] == "" ) {
+				matrix[idx][idy] = alphabet[ Math.floor(Math.random() * alphabet.length + 1)-1 ];
+			}
+			else {
+				matrix[idx][idy] = "<b>" + matrix[idx][idy] + "</b>";
+			}
+		}
+	}
+	
+	// print result matrix for debug purposes
+	document.write("<table>");
+	for( let idx=0; idx < MATRIX_SIZE; idx++ ) 
+	{
+		document.write("<tr>");
+
+		for( let idy=0; idy < MATRIX_SIZE; idy++ ) 
+		{
+			document.write("<td>" + matrix[idx][idy] + "</td>");
+		}
+		document.write("</tr>");
+	}
+	document.write("</table>");
 }
+
+
+//( placeWords(["АЛЬКОР", "ТРУБА", "ОСЬМИЗНАК", "ВЕНТИЛЯТ", "ГРЫМЗА", "ДВА", "КРЫША", "СЕМЕНЫЧ", "ЯБЛОКО", "НЕМЕЦ"]) )();
+
+
+
